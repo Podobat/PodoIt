1d07d1d60d3bfbfe2f4bc807c57d1cb091d1f3de
eden <noedenkim@gmail.com>
2025-08-22 16:46:45 +0900

    chore: #6 - SwiftData Persistence Layer 설정
    Dependencies 패키지 추가

--- PodoIt/Data/SwiftDataManager.swift
+++ PodoIt/Data/SwiftDataManager.swift
@@ -5,3 +5,10 @@
 //  Created by 김이든 on 8/21/25.
 //
 
+import Dependencies
+import Foundation
+import SwiftData
+
+final class SwiftDataManager {
+  @Dependency(\.modelContext) private var modelContext
+}
