c4a05bafea847b235b390812723648967576c4bc
Gwangyong <key4168@icloud.com>
2025-08-22 23:19:35 +0900

    feat: #4 - 기본적은 UITableView 구성
    - UITableView + DataSource로 구현
    - separatorInset = .zero로 디자인과 동일한 구분선 구현
    - 임시 데이터로 테이블 뷰 구성 확인

--- PodoIt/Scenes/Setting/SettingViewController.swift
+++ PodoIt/Scenes/Setting/SettingViewController.swift
@@ -6,19 +6,49 @@
 //
 
 import UIKit
+import SnapKit
 
 final class SettingViewController: UIViewController {
+  
+  private lazy var tableView = UITableView(frame: .zero, style: .plain).then {
+    $0.dataSource = self
+    $0.delegate = self
+    $0.register(UITableViewCell.self, forCellReuseIdentifier: "Cell")
+    $0.separatorInset = .zero // 구분선의 시스템 마진을 zero로
+  }
+  
   // MARK: - Lifecycle
 
   override func viewDidLoad() {
     super.viewDidLoad()
     configureUI()
+    configureLayout()
   }
 
   // MARK: - Private Methods
 
   private func configureUI() {
     view.backgroundColor = .systemBackground
-    title = "설정"
+    view.addSubview(tableView)
+  }
+  
+  private func configureLayout() {
+    tableView.snp.makeConstraints {
+      $0.directionalEdges.equalTo(view.safeAreaLayoutGuide)
+    }
+  }
+}
+
+extension SettingViewController: UITableViewDelegate {}
+
+extension SettingViewController: UITableViewDataSource {
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    3
+  }
+
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    cell.textLabel?.text = "테스트"
+    return cell
   }
 }
