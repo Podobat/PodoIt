21015cc580f712d5352747ac281fb0fd4d883e2e
Gwangyong <key4168@icloud.com>
2025-08-24 16:03:13 +0900

    chore: #4 - View 폴더링 및 Cell 초기 생성
    - View 폴더를 생성하여, View 파일들 정리
    - SettingViewCell 파일을 만들어서 초기 세팅

---
+++ PodoIt/Scenes/Setting/View/SettingViewCell.swift
@@ -0,0 +1,22 @@
+//
+//  SettingViewCell.swift
+//  PodoIt
+//
+//  Created by 서광용 on 8/24/25.
+//
+
+import UIKit
+import SnapKit
+
+final class SettingViewCell: UITableViewCell {
+  
+  override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
+    super.init(style: style, reuseIdentifier: reuseIdentifier)
+  }
+
+  required init?(coder: NSCoder) {
+    fatalError("init(coder:) has not been implemented")
+  }
+  
+  
+}
