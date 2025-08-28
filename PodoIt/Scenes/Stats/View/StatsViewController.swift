cad8b013ea6bc5e15c563eab17828851813e1378
eden <noedenkim@gmail.com>
2025-08-28 20:29:00 +0900

    feat: #32 - StatsViewController 스크롤 구현

--- PodoIt/Scenes/Stats/View/StatsViewController.swift
+++ PodoIt/Scenes/Stats/View/StatsViewController.swift
@@ -5,13 +5,26 @@
 //  Created by 노가현 on 8/20/25.
 //
 
+import Then
+import SnapKit
 import UIKit
 
 final class StatsViewController: UIViewController {
   // MARK: - Properties
 
   private let headerView = StatsHeaderView()
+  private let calendarView = CalendarView()
   private let summaryView = StatsSummaryView()
+
+  private let scrollView = UIScrollView().then {
+    $0.backgroundColor = .gray100
+    $0.alwaysBounceVertical = true
+  }
+
+  private let contentStackView = UIStackView().then {
+    $0.axis = .vertical
+    $0.spacing = 0
+  }
 
   // MARK: - Lifecycle
 
@@ -30,17 +43,25 @@ final class StatsViewController: UIViewController {
   }
 
   private func setupViews() {
-    [headerView, summaryView].forEach { view.addSubview($0) }
+    [headerView, scrollView].forEach { view.addSubview($0) }
+    scrollView.addSubview(contentStackView)
+    [calendarView, summaryView].forEach { contentStackView.addArrangedSubview($0) }
   }
 
   private func setupConstraints() {
     headerView.snp.makeConstraints {
       $0.top.equalTo(view.safeAreaLayoutGuide)
-      $0.leading.trailing.equalToSuperview()
+      $0.directionalHorizontalEdges.equalToSuperview()
     }
-    summaryView.snp.makeConstraints {
+
+    scrollView.snp.makeConstraints {
       $0.top.equalTo(headerView.snp.bottom)
-      $0.leading.trailing.equalToSuperview()
+      $0.directionalHorizontalEdges.bottom.equalToSuperview()
+    }
+
+    contentStackView.snp.makeConstraints {
+      $0.edges.equalToSuperview()
+      $0.width.equalTo(scrollView.snp.width)
     }
   }
 }
