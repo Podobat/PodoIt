1e387d7d54ca08501599a556c9d43d81c38b73f2
eden <noedenkim@gmail.com>
2025-09-01 19:26:22 +0900

    chore: #66 - StatsCategoryModel 수정 및 더미 데이터 추가

--- PodoIt/Scenes/Stats/View/ViewSubClasses/Header/StatsCategoryModel.swift
+++ PodoIt/Scenes/Stats/View/ViewSubClasses/Header/StatsCategoryModel.swift
@@ -8,15 +8,25 @@
 import Foundation
 
 // 카테고리
-struct StatsCategoryModel: Equatable {
+struct StatsCategoryModel: Hashable {
   let name: String
-  let icon: String? // 전체는 nil
+  let icon: String?
   static let all = StatsCategoryModel(name: "전체", icon: nil)
+
+  func hash(into hasher: inout Hasher) {
+    hasher.combine(name) // name만 고유성 판단 기준
+  }
 }
 
 // 더미 데이터
 extension StatsCategoryModel {
   static let items: [StatsCategoryModel] = [
+    .init(name: "공부", icon: "📚"),
+    .init(name: "코딩", icon: "💻"),
+    .init(name: "메모", icon: "📝"),
+    .init(name: "공부", icon: "🔥"), // name 중복 안됨
+    .init(name: "코딩", icon: "🍇"),
+    .init(name: "메모", icon: "🦊"),
     .init(name: "공부", icon: "📚"),
     .init(name: "코딩", icon: "💻"),
     .init(name: "메모", icon: "📝"),
