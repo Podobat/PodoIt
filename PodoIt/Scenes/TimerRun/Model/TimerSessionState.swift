f84b4fea41cc7e2f27ca6db27782b44922b34921
Gwangyong <key4168@icloud.com>
2025-09-02 16:44:38 +0900

    chore: #44 - SwiftFormat 적용

--- PodoIt/Scenes/TimerRun/Model/TimerSessionState.swift
+++ PodoIt/Scenes/TimerRun/Model/TimerSessionState.swift
@@ -9,7 +9,7 @@ import Foundation
 struct TimerSessionState {
   var sessionStart: Date // 공부 시작한 시간 기록
   var stateStart: Date // 현재 구간(공부/휴식)이 시작된 시간 기록
-  var isRunning: Bool  // 공부 중(true)/휴식 중(false)
+  var isRunning: Bool // 공부 중(true)/휴식 중(false)
   var studySeconds: Int // 지금까지 누적된 총 공부 시간(초)
   var restSeconds: Int // 지금까지 누적된 총 휴식 시간(초)
 }
