76cc49be953520d09f79f747a7c38edde319c2a4
Gwangyong <key4168@icloud.com>
2025-09-02 18:14:20 +0900

    refactor: #45 - UI 바인딩을 drive(with:)로 통일 및 self 캡처
    - startPauseTap/stopButtonTap 이벤트를 drive(with:)로 변경하여 UI 바인딩 일관적이도록 수정
    - progress/runningTime/goalTime 등 UI 업데이트 스트림에서도 drive 적용
    - self 캡처 방식을 drive(with:)로 통합

--- PodoIt/Scenes/TimerRun/View/TimerRunViewController.swift
+++ PodoIt/Scenes/TimerRun/View/TimerRunViewController.swift
@@ -88,19 +88,19 @@ final class TimerRunViewController: UIViewController {
   private func bind() {
     // 버튼 Tap을 스트림으로 받아서 viewModel의 토글 실행 (start/pause)
     buttonBarView.startPauseTap
-      .withUnretained(self)
-      .bind(onNext: { vc, _ in
+      .asDriver()
+      .drive(with: self) { vc, _ in
         vc.viewModel.startAndPause()
-      })
+      }
       .disposed(by: disposeBag)
 
     // stop 버튼 tap하여 중지
     buttonBarView.stopButtonTap
-      .withUnretained(self)
-      .bind(onNext: { vc, _ in
+      .asDriver()
+      .drive(with: self) { vc, _ in
         vc.viewModel.stop()
         vc.navigationController?.popViewController(animated: true) // pop되면서 interval도 중지
-      })
+      }
       .disposed(by: disposeBag)
 
     // 총 공부시간 "0:00:00" 진행
@@ -119,19 +119,19 @@ final class TimerRunViewController: UIViewController {
       .asObservable()
       .take(until: rx.methodInvoked(#selector(UIViewController.viewWillDisappear(_:))))
       .asDriver(onErrorJustReturn: 0.0)
-      .drive(onNext: { [middleView] progress in
+      .drive(with: self) { vc, progress in
         if progress >= 0.9999 { // 반올림 생각해서
-          middleView.updateProgressBar(progress: 1.0)
+          vc.middleView.updateProgressBar(progress: 1.0)
         } else {
-          middleView.progressBar.layoutIfNeeded() // 애니메이션 꼬임 방지용. 미리 레이아웃 최신상태로
+          vc.middleView.progressBar.layoutIfNeeded() // 애니메이션 꼬임 방지용. 미리 레이아웃 최신상태로
           let animator = UIViewPropertyAnimator(duration: 1, curve: .linear) // 선형으로 1초마다 애니메이션 객체 생성
           animator.addAnimations {
-            middleView.progressBar.progress = progress // 매 틱 들어오는 진행률(progress) 값 바인딩
-            middleView.progressBar.layoutIfNeeded() // 안하면 툭툭 끊김
+            vc.middleView.progressBar.progress = progress // 매 틱 들어오는 진행률(progress) 값 바인딩
+            vc.middleView.progressBar.layoutIfNeeded() // 안하면 툭툭 끊김
           }
           animator.startAnimation()
         }
-      })
+      }
       .disposed(by: disposeBag)
 
     // 공부 중/휴식 중 상태에 따른 버튼 이미지, 색상 변경
