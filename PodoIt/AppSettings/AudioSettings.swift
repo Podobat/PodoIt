afaef4f4b0b1d1e7efde91ead8e69ded65179491
Gwangyong <key4168@icloud.com>
2025-09-09 11:23:40 +0900

    feat: #50 - AudioSettings 싱글톤 생성
    - mute 상태를 가지고 있을 전역의 AudioSettings를 싱글톤으로 만듦

---
+++ PodoIt/AppSettings/AudioSettings.swift
@@ -0,0 +1,13 @@
+//
+//  AudioSettings.swift
+//  PodoIt
+//
+//  Created by 서광용 on 9/9/25.
+//
+
+final class AudioSettings {
+  static let shared = AudioSettings() // 싱글톤
+  private init() {}
+  
+  
+}
