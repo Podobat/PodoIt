1e39976c6f438df1e2a4496c92f2d149da69e7fa
Gwangyong <key4168@icloud.com>
2025-09-18 20:44:15 +0900

    feat: #192 - 이미지 에셋 변경 및 변경된 UI로 구성
    - UIButton configuration으로 구성하는 방식으로 변경
    - 변경된 이미지 에셋 변경

--- PodoIt/Scenes/TimerRun/View/Sections/ButtonSectionView.swift
+++ PodoIt/Scenes/TimerRun/View/Sections/ButtonSectionView.swift
@@ -12,73 +12,104 @@ import UIKit
 
 final class ButtonSectionView: UIView {
   // MARK: - Components
-
+  
   private let hStackView = UIStackView().then {
     $0.axis = .horizontal
-    $0.spacing = 16
+    $0.spacing = 8
   }
-
-  private lazy var stopButton = UIButton(type: .system).then {
-    $0.backgroundColor = .gray100
-    $0.tintColor = .gray900
-    $0.setImage(UIImage(named: "stop-fill"), for: .normal)
-    $0.layer.cornerRadius = 32 // 버튼이 고정값이라 값 명시
-    $0.clipsToBounds = true
-  }
-
-  private(set) lazy var startPauseButton = UIButton(type: .system).then {
-    $0.backgroundColor = .primary600
-    $0.tintColor = .appWhite
-    $0.setImage(UIImage(named: "play-white"), for: .normal)
-    $0.layer.cornerRadius = 32
-    $0.clipsToBounds = true
-  }
-
+  
+  private lazy var stopButton = UIButton(configuration: {
+    var config = UIButton.Configuration.filled()
+    config.baseBackgroundColor = .gray100
+    config.image = UIImage(named: "stop-fill")
+    config.imagePadding = 8 // 8만큼의 spacing
+    config.attributedTitle = AttributedString("종료하기", attributes: AttributeContainer([
+      .font: Typography.font(for: .labelMd(weight: .medium)),
+      .foregroundColor: UIColor.gray900
+    ]))
+    config.contentInsets = NSDirectionalEdgeInsets(
+      top: 16,
+      leading: 20,
+      bottom: 16,
+      trailing: 20
+    )
+    config.cornerStyle = .fixed
+    return config
+  }())
+  
+  private(set) lazy var startPauseButton = UIButton(configuration: {
+    var config = UIButton.Configuration.filled()
+    config.baseBackgroundColor = .gray100
+    config.image = UIImage(named: "pause")
+    config.imagePadding = 8
+    config.attributedTitle = AttributedString("휴식하기", attributes: AttributeContainer([
+      .font: Typography.font(for: .labelMd(weight: .medium)),
+      .foregroundColor: UIColor.gray900
+    ]))
+    config.contentInsets = NSDirectionalEdgeInsets(
+      top: 16,
+      leading: 20,
+      bottom: 16,
+      trailing: 20
+    )
+    config.cornerStyle = .fixed
+    return config
+  }())
+  
   // MARK: - init
-
+  
   override init(frame: CGRect) {
     super.init(frame: frame)
     configureUI()
     configureLayout()
   }
-
+  
   @available(*, unavailable)
   required init?(coder: NSCoder) {
     fatalError("init(coder:) has not been implemented")
   }
-
+  
+  override func layoutSubviews() {
+    super.layoutSubviews()
+    layoutIfNeeded()
+    stopButton.configuration?.background.cornerRadius = stopButton.bounds.height / 2
+    startPauseButton.configuration?.background.cornerRadius = startPauseButton.bounds.height / 2
+  }
+  
   // MARK: - configureUI
-
+  
   private func configureUI() {
     backgroundColor = .appWhite
     addSubview(hStackView)
     [stopButton, startPauseButton].forEach { hStackView.addArrangedSubview($0) }
   }
-
+  
   // MARK: - configureLayout
-
+  
   private func configureLayout() {
     hStackView.snp.makeConstraints {
       $0.centerX.equalToSuperview()
       $0.top.bottom.equalToSuperview().inset(24)
     }
-
-    for item in [stopButton, startPauseButton] {
-      item.snp.makeConstraints { $0.size.equalTo(64) }
-    }
   }
-
+  
   // MARK: 집중/휴식 상태에 따른 버튼 이미지, 색상 변경
-
+  
   func updateStartPauseButtonImage(isStudying: Bool) {
     if isStudying { // 공부 중
-      startPauseButton.backgroundColor = .gray100
-      startPauseButton.tintColor = .gray900
-      startPauseButton.setImage(UIImage(named: "pause"), for: .normal)
+      startPauseButton.configuration?.baseBackgroundColor = .gray100
+      startPauseButton.configuration?.image = UIImage(named: "pause")
+      startPauseButton.configuration?.attributedTitle = AttributedString("휴식하기", attributes: AttributeContainer([
+        .font: Typography.font(for: .labelMd(weight: .medium)),
+        .foregroundColor: UIColor.gray900
+      ]))
     } else { // 휴식 중
-      startPauseButton.backgroundColor = .primary600
-      startPauseButton.tintColor = .appWhite
-      startPauseButton.setImage(UIImage(named: "play-white"), for: .normal)
+      startPauseButton.configuration?.baseBackgroundColor = .primary600
+      startPauseButton.configuration?.image = UIImage(named: "play-white")
+      startPauseButton.configuration?.attributedTitle = AttributedString("집중하기", attributes: AttributeContainer([
+        .font: Typography.font(for: .labelMd(weight: .medium)),
+        .foregroundColor: UIColor.appWhite
+      ]))
     }
   }
 }
@@ -90,7 +121,7 @@ extension ButtonSectionView {
   var startPauseTap: ControlEvent<Void> {
     return startPauseButton.rx.tap
   }
-
+  
   var stopButtonTap: ControlEvent<Void> {
     return stopButton.rx.tap
   }
